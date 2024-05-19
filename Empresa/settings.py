5ed9c1e7835cf1cd7a6857153d2c095be5e79270
Yeremi Guerrero <yer.guerrero@duocuc.cl>
2024-05-18 20:39:02 -0400

    Se agrega bd sqllite

--- Empresa/settings.py
+++ Empresa/settings.py
@@ -72,7 +72,12 @@ WSGI_APPLICATION = 'Empresa.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 SUPABASE_URL = os.environ.get('SUPABASE_URL')
 SUPABASE_KEY = os.environ.get('SUPABASE_KEY')
