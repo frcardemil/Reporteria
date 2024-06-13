1de3e012a24754ec9a7cbc3433e73667b86ee16f
Yeremi Guerrero <yer.guerrero@duocuc.cl>
2024-06-13 14:33:35 -0400

    Update

--- reporteria/api/serializers.py
+++ reporteria/api/serializers.py
@@ -4,5 +4,4 @@ from reporteria.models import ReporteGeneral
 class ReporteSerializer(ModelSerializer):
     class Meta:
         model = ReporteGeneral
-        fields = ['mes_anno_reporte','entrega_total','entrega_a_tiempo','stock_productos','pedido_proveedor',
-                  'adquisicion_recibida','venta_pedido','venta_realizada','factura_total','factura_pagada']
+        fields = '__all__'
