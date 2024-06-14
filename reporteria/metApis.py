6d12752f9e2d86532fedbbc4e355960fd9efc0dc
Maikel Andres Cisternas Mercado <mai.cisternas@duocuc.cl>
2024-06-14 01:29:33 -0400

    Micro avance 14/06
    -Adaptar el metApi al fast api

--- reporteria/metApis.py
+++ reporteria/metApis.py
@@ -1,23 +1,14 @@
 import requests
-apisHosts = {
-    'adquisiciones' : ['34.199.103.123','/'],
-    'proveedor' : ['0.0.0.0','/'],
-    'ventas' : ['0.0.0.0','/'],
-    'despacho' : ['44.205.221.190','/'],
-    'post_venta' : ['44.220.241.211','/'],
-    'contabilidad' : ['54.159.228.5','/'],
-    'stock' : ['0.0.0.0','/'],
-    'reporteria' : ['34.224.245.239','/'],
-    'seguridad' : ['0.0.0.0','/'],
-}
 
-def requestApis(user,password,area,modelo):
-    url='https://'+apisHosts[area]+':8000'+modelo
-    r = requests.get(url, auth=(user, password))
+def requestApis(area):
+    url='https://172.31.20.240:8000/'+area
+    r = requests.get(url)
     print(r.status_code)
     if r.status_code == 200:
+        print('viva xd')
         return r.json()
     else:
+        print('no viva :c')
         return []
 
 def contarTotal(json):
