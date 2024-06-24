582c0615e110f9b00fc8819450b3f9ecccf6308a
Maikel Andres Cisternas Mercado <mai.cisternas@duocuc.cl>
2024-06-24 01:14:24 -0400

    Update 24/06

--- reporteria/metApis.py
+++ reporteria/metApis.py
@@ -1,20 +1,98 @@
 import requests
+from datetime import datetime
 
-def requestApis(area):
-    url='https://172.31.20.240:8000/'+area
+#44.218.227.202
+#34.224.245.239
+
+apisHosts = {
+    'adquisiciones' : '34.199.103.123:8000',
+    'proveedor' : '0.0.0.0:8000',
+    'ventas' : '0.0.0.0:8000',
+    'despacho' : '44.205.221.190:8000',
+    'post_venta' : '44.220.241.211:8000',
+    'contabilidad' : '54.159.228.5:8000',
+    'stock' : '0.0.0.0:8000',
+    'reporteria' : '44.218.227.202:8000',
+    'seguridad' : '0.0.0.0:8000',
+}
+
+def reporteriaApi():
+    url = 'http://'+apisHosts['reporteria']+'/reporteria/api/reporte/'
     r = requests.get(url)
-    print(r.status_code)
-    if r.status_code == 200:
-        print('viva xd')
-        return r.json()
-    else:
-        print('no viva :c')
-        return []
+    json_data = r.json()
+    return json_data
 
-def contarTotal(json):
-    total = 0
-    return total
+def adquisicionesApi():
+    url = 'http://'+apisHosts['adquisiciones']+'/api/v1/productos/'
+    r = requests.get(url)
+    json_data = r.json()
+    return json_data
 
-def listarDatosDash():
-    lista =[]
-    return lista
+"""
+No poseo ruta exacta de proveedor
+
+def proveedorApi():
+    url = 'http://'+apisHosts['proveedor']+'/proveedor/'
+    r = requests.get(url)
+    json_data = r.json()
+    return json_data
+"""
+
+"""
+No poseo ruta exacta de ventas
+
+def ventasApi():
+    url = 'http://'+apisHosts['ventas']+'/ventas/'
+    r = requests.get(url)
+    json_data = r.json()
+    return json_data
+"""
+
+def despachoApi():
+    url = 'http://'+apisHosts['despacho']+'/despachos/'
+    r = requests.get(url)
+    json_data = r.json()
+    return json_data
+
+"""
+No poseo ruta exacta de post-venta
+
+def post_ventaApi():
+    url = 'http://'+apisHosts['post_venta']+'/post_venta/'
+    r = requests.get(url)
+    json_data = r.json()
+    return json_data
+"""
+
+def contabilidadApi():
+    url = 'http://'+apisHosts['contabilidad']+'/api/Facturas/'
+    r = requests.get(url)
+    json_data = r.json()
+    return json_data
+
+"""
+No poseo ruta exacta de stock
+
+def stockApi():
+    url = 'http://'+apisHosts['stock']+'/stock/'
+    r = requests.get(url)
+    json_data = r.json()
+    return json_data
+"""
+
+"""
+No poseo ruta exacta de seguridad
+
+def seguridadApi():
+    url = 'http://'+apisHosts['seguridad']+'/seguridad/'
+    r = requests.get(url)
+    json_data = r.json()
+    return json_data
+"""
+
+def contarJson(json):
+    filtered_data = [
+        item for item in json 
+        if 'datefield' in item and datetime.strptime(item['datefield'], '%Y-%m-%d').year == target_year
+    ]
+    return filtered_data
